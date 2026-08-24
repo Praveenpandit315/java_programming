449cda36b0c978753b57801eccc980cf25bdc27c
Praveen Gautam <praveengautam433603@gmail.com>
2026-08-24 15:37:37 +0530

    Plainedrome Program

---
+++ Day6/Plainedrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Plainedrome {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a no.: ");
+        int num=sc.nextInt();
+        int temp = num;
+        int sum=0;
+        while(num>0){
+            int ld=num%10;
+            sum=sum*10+ld;
+            num=num/10;
+        }
+        if( temp == sum){
+            System.out.println("Planedrome" + sum);
+        }
+        else{
+            System.out.println("Not Plainedrome");
+        }
+    
+    }
+}
