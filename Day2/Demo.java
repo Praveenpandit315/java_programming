b9742fa4580e7678b5ce468317a20bd784901540
Praveen Gautam <praveengautam433603@gmail.com>
2026-08-25 20:22:41 +0530

    Demo

--- Day2/Demo.java
+++ Day2/Demo.java
@@ -1,1 +1,18 @@
+public class Demo{
+    static int x=10;
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        InnerDemo obj = new InnerDemo();
+        obj.display();
+    }
+}
+// Interpreter compiler and bytecode class file ko bolte hai or plateform independent 
 
+/**
+ * InnerDemo
+ */
+class InnerDemo {
+    void display(){
+        Demo.x=20;
+    }    
+}
