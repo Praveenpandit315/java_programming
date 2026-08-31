2648f51d9f2a7579f69b58e04c651e402c7a9afb
Praveen Gautam <praveengautam433603@gmail.com>
2026-08-31 15:05:07 +0530

    Add files via upload

--- Day8/method.java
+++ Day8/method.java
@@ -1,24 +1,32 @@
+import java.util.Scanner;
+
 public class method {
 
-    public static void printDollar(int row) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Row");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            pattern.Dollar(i);
+            pattern.Hash(n, i);
+            System.out.println();
+        }
+    }
+}
+
+class pattern{
+    
+    public static void Dollar(int row) {
         for (int j = 1; j <= row; j++) {
             System.out.print("$ ");
         }
     }
 
-    public static void printHash(int n, int row) {
+    public static void Hash(int n, int row) {
         for (int j = 1; j <= n - row; j++) {
             System.out.print("# ");
         }
     }
 
-    public static void main(String[] args) {
-        int n = 5;
-
-        for (int i = 0; i < n; i++) {
-            printDollar(i);
-            printHash(n, i);
-            System.out.println();
-        }
-    }
 }
